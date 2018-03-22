71240a1e5f93d2036976f39b446f9192bc47e975
Pinklotus15 <36045743+Pinklotus15@users.noreply.github.com>
2018-03-22 09:57:39 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-
-function setup() {
+function setup(){
   createCanvas(400, 400);
 }
-
-function draw() {
-	fill(mouseX,mouseY,100);
-	ellipse(mouseX,mouseY,100,100);}
+function draw(){
+fill(mouseX,mouseY,100);
+ellipse(mouseX,mouseY,100,100);
+}
   
   
